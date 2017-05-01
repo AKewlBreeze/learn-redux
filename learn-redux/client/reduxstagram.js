f8d6ed981cb45c62217732cb60cdb02e9a4e3cc9
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-01 08:23:54 -0600

    add

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { render } from 'react-dom';
+import { render } from "react-dom";
 
-import css from './styles/style.styl';
+import css from "./styles/style.styl";
 
-// render(<p>hi</p>, document.getElementById('root'));
+import Main from "./components/Main";
 
-import Main from './components/Main';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
+import Single from "./components/Single";
 
-import { Router, Route, IndexRoute, browserHistory} from 'react-rotuer';
+import PhotoGrid from "./components/PhotoGrid";
+
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
 const router = (
-  <Router history={browerHistory}>
-    <Route path="/" compoenent={Main}>
-    <IndexRoute component={PhotoGrid}></IndexRoute>
-    <Route path="/view/:postId" component={Single}></Route>
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
     </Route>
   </Router>
 );
 
-render(<Main/>, document.getElementById('root'));
+render(router, document.getElementById("root"));
